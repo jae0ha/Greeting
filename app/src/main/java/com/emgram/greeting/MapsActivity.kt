50b4d397a94af4fc10859679d8005328b613bf83
jae0ha <jae0ha@gmail.com>
2024-01-11 01:40:02 +0900

    현재 위치 지도 찍기 기본기능 수정

--- app/src/main/java/com/emgram/greeting/MapsActivity.kt
+++ app/src/main/java/com/emgram/greeting/MapsActivity.kt
@@ -34,7 +34,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
-        // FusedLocationProviderClient 초기화
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         // SupportMapFragment를 가져와서 지도가 준비되면 알림을 받습니다.
@@ -57,6 +56,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // 1. 기본 설정된 위치로 지도 위치를 설정하기
         // 지도에 표시할 마커의 위치를 추가
+        /*
         val hanlasan = LatLng(33.362361, 126.533277)
         mMap.addMarker(MarkerOptions().position(hanlasan).title("Marker in hanla mt"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(hanlasan))
@@ -64,83 +64,88 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // 지도 확대하기
         val zoomLevel = 12f
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hanlasan, zoomLevel))
-        
+        */
         // 2. FusedLocationProviderCient를 사용해 사용위치 업데이트 하기
+
         getLocationPermission()
+        updateLocationUI()
+        getDeviceLocation()
+
     }
-    
-    // 실시간 사용자 위치 가져오기
-    /*
-     런타임 권한 요청
-     안드로이드 6.0 (API레벨 23) 이상에서는 런타임에 위치 권한을
-     요청해야 한다.
-     */
-    
-    // 사용자에게 위치 권한을 요청하는 코드를 추가
-    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    private val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
 
     private fun getLocationPermission() {
         if (ContextCompat.checkSelfPermission(this.applicationContext,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // 권한이 이미 부여된 경우
-            onLocationPermissionGranted()
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true
         } else {
-            // 권한 요청
             ActivityCompat.requestPermissions(this,
                 arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-                LOCATION_PERMISSION_REQUEST_CODE)
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
         }
     }
 
-    private fun onLocationPermissionGranted() {
-        // 권한이 부여된 후에 수행할 작업을 여기에 구현
-        getDeviceLocation()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            LOCATION_PERMISSION_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 사용자가 권한을 부여한 경우
-                    onLocationPermissionGranted()
-                } else {
-                    // 사용자가 권한을 거부한 경우
-                    // 권한 거부에 대한 처리 로직
-                }
-                return
-            }
-        }
-    }
-
-
-    // FusedLocationProviderClient 사용 위치 업데이트
-    // FusedLocationProviderClient 의 의존성을 추가해야 한다.
     private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
 
     private fun getDeviceLocation() {
         try {
-            val locationResult = mFusedLocationProviderClient.lastLocation
-            locationResult.addOnCompleteListener(this) { task ->
-                if(task.isSuccessful) {
-                    // 현재 위치를 찾은 경우, 위치 정보 세팅
-                    val lastKnownLocation = task.result
-                    if (lastKnownLocation != null) {
-                        val currentPlace = LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
-                        mMap.addMarker(MarkerOptions().position(currentPlace).title("You are here!!"))
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                            LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude), 8f)
-                        )
+            if (mLocationPermissionGranted) {
+                val locationResult = mFusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 현재 위치를 찾은 경우, 위치 정보를 사용합니다.
+                        val lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation.latitude,
+                                    lastKnownLocation.longitude), 12f))
+                        }
+                    } else {
+                        // 현재 위치를 찾지 못한 경우, 기본 위치를 사용하고, 예외 로그를 출력합니다.
+                        Log.e(TAG, "Exception: %s", task.exception)
+                        mMap.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(LatLng(33.362361, 126.533277), 12f))
+                        mMap.uiSettings.isMyLocationButtonEnabled = false
                     }
-                } else {
-                    // 현재 위치를 찾지 못한 경우, 한라산을 찍어 줍시다.
-                    val hanlasan = LatLng(33.362361, 126.533277)
-                    mMap.addMarker(MarkerOptions().position(hanlasan).title("Cannot find your place goto hanla"))
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hanlasan, 8f))
                 }
             }
         } catch (e: SecurityException) {
             Log.e("Exception: %s", e.message, e)
         }
     }
+
+    private fun updateLocationUI() {
+        if (mMap == null) {
+            return
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.isMyLocationEnabled = true
+                mMap.uiSettings.isMyLocationButtonEnabled = true
+            } else {
+                mMap.isMyLocationEnabled = false
+                mMap.uiSettings.isMyLocationButtonEnabled = false
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        mLocationPermissionGranted = false
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true
+                }
+            }
+        }
+        updateLocationUI()
+        getDeviceLocation()
+    }
+
+
 }
